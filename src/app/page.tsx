355373f64b0ca6547975aa656586ca7852c0880d
priyanshusinha11 <priyanshusinha.2807@gmail.com>
2024-08-01 20:30:04 +0530

    nav init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 
 export default function Home() {
   return (
-    <h1 className="text-2xl text-center">Testing</h1>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
+      <h1 className="text-2xl text-center">Testing</h1>
+    </main>
   );
 }
