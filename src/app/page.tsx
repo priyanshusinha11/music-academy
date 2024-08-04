d31bd1c4e98b222808fb5ae8334242e8ecc89105
priyanshusinha11 <priyanshusinha.2807@gmail.com>
2024-08-05 02:06:22 +0530

    hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
       <h1 className="text-2xl text-center">Testing</h1>
+      <HeroSection/>
     </main>
   );
 }
