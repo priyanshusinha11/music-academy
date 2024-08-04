3d9e3c94bce5ea61d36c9baf04aed5aff900f0e8
priyanshusinha11 <priyanshusinha.2807@gmail.com>
2024-08-05 02:14:14 +0530

    sticky scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/HeroSection";
 import FeaturedCourses from "@/components/FeaturedCourses";
+import WhyChooseUs from "@/components/WhyChooseUs";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
       <h1 className="text-2xl text-center">Testing</h1>
       <HeroSection/>
       <FeaturedCourses/>
+      <WhyChooseUs/>
     </main>
   );
 }
