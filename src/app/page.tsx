afd774a9ce9be64f0e7b22eb0c68b55bd49adc22
priyanshusinha11 <priyanshusinha.2807@gmail.com>
2024-08-05 02:22:33 +0530

    image moving cards for instructors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import FeaturedCourses from "@/components/FeaturedCourses";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
+import Instructors from "@/components/Instructors";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
@@ -12,6 +13,7 @@ export default function Home() {
       <WhyChooseUs/>
        <MusicSchoolTestimonials />
        <UpcomingWebinars/>
+       <Instructors/>
     </main>
   );
 }
